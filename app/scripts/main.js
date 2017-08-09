c5e66f56eaf7c7f7c454bb8417b4f6eaa129a21c
cjkmerriman <callum.merriman@crowdsurge.com>
2017-08-09 10:03:04 -0400

    first commit: work in progress

---
+++ app/scripts/main.js
@@ -0,0 +1,32 @@
+
+var offsetTop;
+var opacity = 0.0;
+var position;
+
+$(document).ready(function() {
+
+	console.log('Thanks for stopping by!');
+
+	var offset = $('.story').offset();
+	offsetTop = offset.top - 160;
+
+	$(window).scroll(function(){
+		
+		position = $(window).scrollTop();
+		opacity = (position - offsetTop) / 200;
+		
+		if (opacity <=0) {
+			$('.profile h2').css('opacity', 0);
+		} else if (opacity <= 1) {
+			$('.profile h2').css('opacity', opacity);
+		} else if (opacity > 1) {
+			$('.profile h2').css('opacity', 1);
+		}
+
+
+	});
+
+	$('video').delay( 14000 ).fadeOut( 1600 );
+
+});
+
