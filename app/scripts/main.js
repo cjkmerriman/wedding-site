1039e692fc8a891defd8e2abb500ee58d6f4924a
Callum <callumm@spotify.com>
2018-03-03 19:37:55 -0500

    Version 2 with RSVP

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
 
 
-	$("#rsvpAction").animatedModal({
+	$('#rsvpAction').animatedModal({
 		modalTarget:'rsvp',
 		animatedIn:'slideInUp',
 		animatedOut:'slideOutDown',
@@ -96,11 +96,11 @@ function initializeForm() {
 		var val = $input.val();
 		$input.addClass('selected')
 
-		if (val == "Joyfully Accept") {
-			$input.val("Regretfully Decline");
+		if (val == 'Joyfully Accept') {
+			$input.val('Regretfully Decline');
 			$input.parent().addClass('regret').removeClass('joy');
 		} else {
-			$input.val("Joyfully Accept");
+			$input.val('Joyfully Accept');
 			$input.parent().addClass('joy').removeClass('regret');
 		}
 	});
@@ -140,7 +140,7 @@ function initializeForm() {
 
 	$form.on('submit', function(e) {
 		e.preventDefault();
-		$('#submit').prop("disabled",true);
+		$('#submit').prop('disabled',true);
 
 		var attendees = $('input[name=attendees]:checked').val();
 		var response = $('input[name=response]').val();
@@ -150,7 +150,7 @@ function initializeForm() {
 		var name4 = $('input[name=name4]').val();
 		var name5 = $('input[name=name5]').val();
 		var message = $('textarea[name=message]').val();
-		var song =  $('input[name=song]').val() + " - " + $('input[name=artist]').val();
+		var song =  $('input[name=song]').val() + ' - ' + $('input[name=artist]').val();
 
 		var formData = {
       'response' : response,
@@ -166,12 +166,12 @@ function initializeForm() {
 
 		var jqxhr = $.ajax({
     	url: formURL,
-    	method: "GET",
-    	dataType: "json",
+    	method: 'GET',
+    	dataType: 'json',
     	data: formData
   	}).done( function(data) {
     	submittedForm(name1, response);
-			$('#submit').prop("disabled",false);
+			$('#submit').prop('disabled',false);
   	});
 	});
 }
@@ -183,7 +183,7 @@ function updateNames(attendees) {
 			$(this).addClass('name-show');
 		} else {
 			$(this).removeClass('name-show');
-			$(this).children('input').val("");
+			$(this).children('input').val('');
 		}
 		count++
 	});
@@ -207,7 +207,7 @@ function submittedForm(name, response) {
 	var firstname = name.substr(0,name.indexOf(' '));
 	//$('.part3 h2 span').html(firstname);
 
-	if (response == "Joyfully Accept") {
+	if (response == 'Joyfully Accept') {
 		$('.not-attending').hide();
 	} else {
 		$('.attending').hide();
@@ -228,7 +228,7 @@ function submittedForm(name, response) {
 // 	}
 // }
 
-!function(a){a.fn.animatedModal=function(n){function o(){m.css({"z-index":e.zIndexOut}),e.afterClose()}function t(){e.afterOpen()}var i=a(this),e=a.extend({modalTarget:"animatedModal",position:"fixed",width:"100%",height:"100%",top:"0px",left:"0px",zIndexIn:"9999",zIndexOut:"-9999",color:"#39BEB9",opacityIn:"1",opacityOut:"0",animatedIn:"zoomIn",animatedOut:"zoomOut",animationDuration:".6s",overflow:"auto",beforeOpen:function(){},afterOpen:function(){},beforeClose:function(){},afterClose:function(){}},n),d=a(".close-"+e.modalTarget),s=a(i).attr("href"),m=a("body").find("#"+e.modalTarget),l="#"+m.attr("id");m.addClass("animated"),m.addClass(e.modalTarget+"-off");var r={position:e.position,width:e.width,height:e.height,top:e.top,left:e.left,"background-color":e.color,"overflow-y":e.overflow,"z-index":e.zIndexOut,opacity:e.opacityOut,"-webkit-animation-duration":e.animationDuration,"-moz-animation-duration":e.animationDuration,"-ms-animation-duration":e.animationDuration,"animation-duration":e.animationDuration};m.css(r),i.click(function(n){n.preventDefault(),a("body, html").css({overflow:"hidden"}),s==l&&(m.hasClass(e.modalTarget+"-off")&&(m.removeClass(e.animatedOut),m.removeClass(e.modalTarget+"-off"),m.addClass(e.modalTarget+"-on")),m.hasClass(e.modalTarget+"-on")&&(e.beforeOpen(),m.css({opacity:e.opacityIn,"z-index":e.zIndexIn}),m.addClass(e.animatedIn),m.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",t)))}),d.click(function(n){n.preventDefault(),a("body, html").css({overflow:"auto"}),e.beforeClose(),m.hasClass(e.modalTarget+"-on")&&(m.removeClass(e.modalTarget+"-on"),m.addClass(e.modalTarget+"-off")),m.hasClass(e.modalTarget+"-off")&&(m.removeClass(e.animatedIn),m.addClass(e.animatedOut),m.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",o))})}}(jQuery);
+!function(a){a.fn.animatedModal=function(n){function o(){m.css({'z-index':e.zIndexOut}),e.afterClose()}function t(){e.afterOpen()}var i=a(this),e=a.extend({modalTarget:'animatedModal',position:'fixed',width:'100%',height:'100%',top:'0px',left:'0px',zIndexIn:'9999',zIndexOut:'-9999',color:'#39BEB9',opacityIn:'1',opacityOut:'0',animatedIn:'zoomIn',animatedOut:'zoomOut',animationDuration:'.6s',overflow:'auto',beforeOpen:function(){},afterOpen:function(){},beforeClose:function(){},afterClose:function(){}},n),d=a('.close-'+e.modalTarget),s=a(i).attr('href'),m=a('body').find('#'+e.modalTarget),l='#'+m.attr('id');m.addClass('animated'),m.addClass(e.modalTarget+'-off');var r={position:e.position,width:e.width,height:e.height,top:e.top,left:e.left,'background-color':e.color,'overflow-y':e.overflow,'z-index':e.zIndexOut,opacity:e.opacityOut,'-webkit-animation-duration':e.animationDuration,'-moz-animation-duration':e.animationDuration,'-ms-animation-duration':e.animationDuration,'animation-duration':e.animationDuration};m.css(r),i.click(function(n){n.preventDefault(),a('body, html').css({overflow:'hidden'}),s==l&&(m.hasClass(e.modalTarget+'-off')&&(m.removeClass(e.animatedOut),m.removeClass(e.modalTarget+'-off'),m.addClass(e.modalTarget+'-on')),m.hasClass(e.modalTarget+'-on')&&(e.beforeOpen(),m.css({opacity:e.opacityIn,'z-index':e.zIndexIn}),m.addClass(e.animatedIn),m.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',t)))}),d.click(function(n){n.preventDefault(),a('body, html').css({overflow:'auto'}),e.beforeClose(),m.hasClass(e.modalTarget+'-on')&&(m.removeClass(e.modalTarget+'-on'),m.addClass(e.modalTarget+'-off')),m.hasClass(e.modalTarget+'-off')&&(m.removeClass(e.animatedIn),m.addClass(e.animatedOut),m.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',o))})}}(jQuery);
 
 function toProperCase(s) {
   return s.toLowerCase().replace(/^(.)|\s(.)/g,
